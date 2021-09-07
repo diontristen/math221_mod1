b8a6fbdfc1031a7c0c3b5a95362436de7b7ce004
diontristen <jontristen8@gmail.com>
2021-09-07 22:10:32 +0800

    feat: New Lagrange Comptuation

--- src/computation/langrange.jsx
+++ src/computation/langrange.jsx
@@ -2,12 +2,11 @@ import {
     evaluate, simplify, round, multiply
 } from 'mathjs'
 import nerdamer from 'nerdamer'
-import { coeffs } from "nerdamer"
 import Polynomial from 'polynomial'
+
 
 Polynomial.trace = true;
 export const computeLagrange = (data, x, decimal) => {
-    console.log('data',data)
     let numberOfDataPoints = data.length
 
     let results = []
@@ -16,14 +15,13 @@ export const computeLagrange = (data, x, decimal) => {
         let numerator = computeNumerator(data, i, numberOfDataPoints)
         let denominator = computeDenominator(data, i, numberOfDataPoints)
         denominator = round(denominator.text(), decimal)
-        let simplified = round(simplifyFraction(data[i][1], denominator, decimal) ,decimal)
-
+        let simplified = round(simplifyFraction(data[i][1], denominator, decimal), decimal)
 
         let lagrange = {
             numerator: numerator,
             denominator: denominator,
             simplified,
-            equation: Polynomial(numerator).mul(simplified).toString()
+            equation: getEquation(numerator, simplified, decimal)
         }
 
         results.push(lagrange)
@@ -32,7 +30,6 @@ export const computeLagrange = (data, x, decimal) => {
     let answer = getFinalEquation(results, decimal)
     let final = Polynomial(answer).eval(x)
     final = round(final, decimal)
-    console.log('final', final)
     return {
         answer,
         results,
@@ -41,36 +38,43 @@ export const computeLagrange = (data, x, decimal) => {
 }
 
 
+const getEquation = (numerator, simplified, decimal) => {
+    let newNumerator = Polynomial(numerator)
+    let newCoeffs = newNumerator.coeff
+    let newEquationArray = []
+    Object.values(newCoeffs).forEach((value, index) => {
+        let solve = round(value * simplified, decimal)
+        newEquationArray.push(solve)
+    })
+    let coeff = Object.assign({}, newEquationArray)
+    return parseEquation(coeff, decimal)
+}
+
 const getFinalEquation = (data, decimal) => {
     let answer = ""
 
     data.forEach((item, index) => {
         let tempEquation = Polynomial(item.equation).coeff
         let equation = parseEquation(tempEquation, decimal)
-  
-        console.log('dion 2', equation, index)
+
         if (index === 0) {
             answer = equation
         } else {
-            console.log('dion final 0', answer)
-            console.log('dion final 0', equation)
-            console.log('dion final 0', item.equation)
             let tempAnswer = Polynomial(answer).add(equation)
-            let yans = tempAnswer.toString()
-            console.log('dion final XX',yans)
             answer = parseEquation(tempAnswer.coeff, decimal)
-            console.log('dion final 2',answer)
-            // answer = Polynomial(answer).add(item.equation).toString()
-            // console.log('dion final 1', answer)
-           
+
         }
     })
+    // console.log('dion ', answer)
 
-    let coeff = Polynomial(answer).coeff
-    // console.log('dion  final', answer, Polynomial(answer))
-    // console.log(Polynomial.trace.map(x => x.toString()))
-    let equation = parseEquation(coeff, decimal)
-    return equation
+    // let coeff = Polynomial(answer).coeff
+    // let equation = parseEquation(coeff, decimal)
+
+
+    let x = Polynomial(answer)
+    console.log('dion' ,x)
+
+    return answer
 }
 
 const parseEquation = (coeff, decimal) => {
@@ -78,23 +82,22 @@ const parseEquation = (coeff, decimal) => {
     let result = ''
     Object.values(coeff).reverse().forEach((value, index) => {
         let number = value.toString()
-        let operator = number.substring(0,1) === '-' ? '-' : '+'
+        let operator = number.substring(0, 1) === '-' ? '-' : '+'
         if (index !== 0) {
-            number = number.substring(0,1) === '-' ?     number.substring(1) : number
+            number = number.substring(0, 1) === '-' ? number.substring(1) : number
         }
-       
+
         let suffix = ''
         if (index !== maxOrder) {
-            suffix =  maxOrder - index > 1 ? `x^${maxOrder - index}` : 'x' 
+            suffix = maxOrder - index > 1 ? `x^${maxOrder - index}` : 'x'
         }
         if (result === '') {
-            result = `${round(number,decimal)}${suffix}`
+            result = `${round(number, decimal)}${suffix}`
         } else {
-            result = `${result}${operator}${round(number,decimal)}${suffix}` 
+            result = `${result}${operator}${round(number, decimal)}${suffix}`
 
         }
     })
-    console.log('dion 4', result)
     return result
 }
 
@@ -104,15 +107,17 @@ const computeNumerator = (data, index, total) => {
     for (let i = 0; i < total; i++) {
         if (i !== index) {
             let number = data[i][0].toString()
-            let operator = number.substring(0,1) === '-' ? '+' : '-'
-            number = number.substring(0,1) === '-' ? number.substring(1) : number
+            let operator = number.substring(0, 1) === '-' ? '+' : '-'
+            number = number.substring(0, 1) === '-' ? number.substring(1) : number
             if (numerator === '') {
                 numerator = `x${operator}${number}`
             } else {
                 numerator = Polynomial(numerator).mul(`x${operator}${number}`).toString()
+
             }
         }
     }
+
     return numerator
 }
 
